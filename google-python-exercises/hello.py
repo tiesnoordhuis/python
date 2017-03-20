ffaf4a8b06c43dfc0163b48e325f0e4cacfa9be1
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-03-20 13:01:16 +0100

    oefeningen

--- google-python-exercises/hello.py
+++ google-python-exercises/hello.py
@@ -26,7 +26,8 @@ def main():
     name = sys.argv[1]
   else:
     name = 'World'
-  print 'Hello', name
+  print 'Howdy', name
+  print 'yay!'
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
